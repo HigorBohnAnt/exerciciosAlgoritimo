58083031d0903d0a0567e85ba65c4812fcba42b7
Higor <higorbohn@gamil.com>
2026-03-28 19:12:57 -0300

    Primeiro commit

---
+++ 1.py
@@ -0,0 +1,7 @@
+num = int(input("Digite um numero: "))
+if num == 0:
+    print("Esse numero é igual a zero")
+elif num > 0:
+    print("Esse numero é positivo")
+else: 
+    print("Esse numero é negativo")
